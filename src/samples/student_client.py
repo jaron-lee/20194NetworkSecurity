e758c1abb264822bcee016081d8bbb036d9e608b
Jaron Lee <jaron2005@gmail.com>
2019-09-13 15:26:13 -0400

    added game instruct

--- src/samples/student_client.py
+++ src/samples/student_client.py
@@ -1,8 +1,16 @@
 import asyncio
+import time
 
 class StudentClient(asyncio.Protocol):
     def __init__(self):
-        pass
+        self.instruction_counter = 0
+        self.instructions = [
+                            "look mirror", 
+                            "get hairpin",
+                            "unlock chest with hairpin",
+                            "get hammer from chest",
+                            "unlock door with hairpin",
+                            "open door"]
 
     def connection_made(self, transport):
         self.transport = transport
@@ -11,12 +19,29 @@ class StudentClient(asyncio.Protocol):
     def data_received(self, data):
         text = data.decode()
         print(text)
+        time.sleep(.2)
         if text == "SUBMIT autograde command:<EOL>\n":
             print("Client: submit request")
             self.transport.write("SUBMIT,Jaron Lee,jaron.lee@jhu.edu,9,1092".encode())
         if text.startswith("SUBMIT: OK"):
             test_id = text.split(",")[1]
             print("Client: success, {}".format(test_id))
+        else:
+            if self.instruction_counter < len(self.instructions):
+
+
+
+                instruction = self.instructions[self.instruction_counter] + "<EOL>\n"
+                print("Client: {}".format(instruction))
+                self.transport.write(instruction.encode())
+            else:
+                print("RAN OUT OF INSTRUCTIONS!")
+
+
+
+            
+
+            
          
 
 if __name__ == "__main__":
