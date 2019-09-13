1a42ffe6ed08f8a67bffa2379fea3b636398d6aa
Jaron Lee <jaron2005@gmail.com>
2019-09-13 15:38:10 -0400

    added client

--- src/samples/student_server.py
+++ src/samples/student_server.py
@@ -1,20 +1,47 @@
 import asyncio
+import functools
+from escape_room_001 import *
+def write_function(string, conn):
+    conn.write(string.encode())
 
 class StudentServer(asyncio.Protocol):
     def __init__(self):
         pass
+        #while game.status == "playing":
+        #    data = conn.recv(1024)
+        #    data_as_string = data.decode()
+        #    lines = data_as_string.split("\n")
+        #    for line in lines:
+        #        if len(line) > 0:
+        #            print(line)
+        #            output = game.command(line)
 
     def connection_made(self, transport):
         self.transport = transport
+        game = EscapeRoomGame(output=functools.partial(write_function, conn=self.transport))
+        game.create_game()
+        game.start()
+        self.game = game
 
     def data_received(self, data):
-        print(data)
-        self.transport.write(data)
+
+        text = data.decode()
+        lines = text.split("<EOL>\n")
+        if self.game.status == "playing":
+            for line in lines:
+                if len(line) > 0:
+                    print(line)
+                    output = self.game.command(line)
+
+        
+        #self.transport.write(data)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     coro = loop.create_server(StudentServer,'',1092)
     server = loop.run_until_complete(coro)
+
+    
 
     try:
         loop.run_forever()
