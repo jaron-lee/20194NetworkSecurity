b87a7649c579ff1c0e3bb61405b7646c2784d90f
Jaron Lee <jaron2005@gmail.com>
2019-09-14 11:46:35 -0400

    trying to run code

--- src/samples/flying_server.py
+++ src/samples/flying_server.py
@@ -1,7 +1,7 @@
 import asyncio
 import time
 import functools
-from escape_room_001 import *
+from escape_room_004 import *
 
 def write_function(string, conn):
     print("SS: ", string)
@@ -15,13 +15,13 @@ class StudentServer(asyncio.Protocol):
     def connection_made(self, transport):
         print("S: connection made")
         self.transport = transport
-        #loop = asyncio.get_event_loop()
+        loop = asyncio.get_event_loop()
         game = EscapeRoomGame(output=functools.partial(write_function, conn=self.transport))
         game.create_game()
         game.start()
         self.game = game
 
-        #await asyncio.wait([asyncio.ensure_future(a) for a in self.game.agents])
+        await asyncio.wait([asyncio.ensure_future(a) for a in self.game.agents])
     def connection_lost(self, ex):
         print("S: closing transport")
         self.transport.close()
@@ -29,7 +29,7 @@ class StudentServer(asyncio.Protocol):
     def data_received(self, data):
         text = data.decode()
         print("SR: ", text)
-        #asyncio.sleep(.2)
+        asyncio.sleep(.2)
         lines = text.split("<EOL>\n")
         if self.game.status == "playing":
             for line in lines:
