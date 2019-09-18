3a924632daa7a7637bfe44137c5d4b70a2993d04
Jaron Lee <jaron2005@gmail.com>
2019-09-17 23:26:00 -0400

    check score

---
+++ src/samples/check_score.py
@@ -0,0 +1,15 @@
+import socket
+import sys
+
+def main(args):
+    HOST = "192.168.200.52"
+    PORT = args[0]
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        data = s.recv(1024).decode()
+        s.send("RESULT,{}".format(args[1]).encode())
+        data = s.recv(1024).decode()
+        print(data)
+
+if __name__ == "__main__":
+    main(sys.argv)
