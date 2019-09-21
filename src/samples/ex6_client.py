fad3ed097812b38a22f4c3f09c884096e9afe148
Jaron Lee <jaron2005@gmail.com>
2019-09-21 15:41:19 -0400

    server code

---
+++ src/samples/ex6_client.py
@@ -0,0 +1,91 @@
+import asyncio
+import playground
+import time
+from autograder_ex6_packets import *
+from playground.network.packet import PacketType
+
+from playground.common.logging import EnablePresetLogging, PRESET_DEBUG
+EnablePresetLogging(PRESET_DEBUG)
+
+class StudentClient(asyncio.Protocol):
+    def __init__(self):
+        self.instruction_counter = 0
+        self.instructions = [
+                            "look mirror", 
+                            "get hairpin",
+                            "unlock chest with hairpin",
+                            "open chest",
+                            "get hammer from chest",
+                            "hit flyingkey with hammer",
+                            "get key",
+                            "unlock door with key",
+                            "open door"]
+
+    def connection_made(self, transport):
+        self.transport = transport
+        d = PacketType.Deserializer()
+        self.d = d
+        start_packet = AutogradeStartTest(
+                name="Jaron Lee",
+                team=9,
+                email="jaron.lee@jhu.edu",
+                port=7816
+                )
+        with open("ex6_game_packet_types.py", "rb") as f:
+            packet.packet_file = f.read()
+        self.transport.write(start_packet.__serialize__())
+        print("Sent packet")
+        #self.transport.write("Hello World".encode())
+
+    def data_received(self, data):
+        print("C: Received packet")
+        self.d.update(data)
+        for packet in self.d.nextPackets():
+            if packet.DEFINITION_IDENTIFIER == "20194.exercise6.autogradesubmitresponse":
+                print("C: SUBMITRESPONSE {} {} {}".format(packet.submit_status, packet.client_status, packet.server_status)
+                #if packet.submit_status != AutogradeTestStatus.PASSED:
+                #    print(packet.error)
+            elif packet.DEFINITION_IDENTIFIER == "exercise6.jaron.response":
+                text = packet.server_response
+                print(text)
+                if packet.game_over():
+                    print("C: GAME OVER - safe to terminate")
+
+                elif self.instruction_counter < len(self.instructions):
+                    #if self.instruction_counter < len(self.instructions):
+                    if self.instructions[self.instruction_counter] == "hit flyingkey with hammer":
+                        if text.split("<EOL>\n")[0].endswith("wall"):
+                            instruction = self.instructions[self.instruction_counter] + "<EOL>\n"
+                            self.transport.write(
+                                    GameCommandPacket(
+                                        command=instruction
+                                        ).__serialize__()
+                                    )
+                            self.instruction_counter += 1
+                    else:
+                        instruction = self.instructions[self.instruction_counter] + "<EOL>\n"
+
+                        print("C: {}".format(instruction))
+                        self.transport.write(
+                                GameCommandPacket(
+                                    command=instruction
+                                    ).__serialize__()
+                                )
+                        self.instruction_counter += 1
+
+
+
+if __name__ == "__main__":
+
+    loop = asyncio.get_event_loop()
+    coro = playground.create_connection(StudentClient,'20194.0.0.19000',19006)
+    client = loop.run_until_complete(coro)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    client.close()
+    loop.run_until_complete(client.close())
+    loop.close()
---
+++ src/samples/ex6_server.py
@@ -0,0 +1,81 @@
+import asyncio
+import time
+import functools
+from escape_room_006 import *
+import playground
+from ex6_game_packet_types import *
+
+from playground.common.logging import EnablePresetLogging, PRESET_VERBOSE
+EnablePresetLogging(PRESET_VERBOSE)
+
+def new_write_function(string, transport, status):
+    string = string + "<EOL>\n"
+    transport.write(
+            GameResponsePacket(
+                server_response = string,
+                server_status=status
+                ).__serialize__()
+            )
+    print(string)
+
+class StudentServer(asyncio.Protocol):
+    def __init__(self):
+        print("S: server started")
+        self.responses = []
+        self.status = None
+
+    #async def wait_agents(self):
+    #    await asyncio.wait([asyncio.create_task(a) for a in self.game.agents])
+
+    def connection_made(self, transport):
+        print("S: connection made")
+        self.transport = transport
+        game = EscapeRoomGame(output=functools.partial(new_write_function, transport=self.transport, status = self.status))
+        game.create_game()
+        game.start()
+        self.game = game
+        #asyncio.create_task(self.wait_agents())
+        for a in game.agents:
+            asyncio.ensure_future(a)
+        self.d = PacketType.Deserializer()
+        print("S: packet sent")
+
+    def connection_lost(self, ex):
+        print("S: closing transport")
+        self.transport.close()
+
+    def data_received(self, data):
+        self.d.update(data)
+        for packet in self.d.nextPackets():
+            if packet.DEFINITION_IDENTIFIER == "exercise6.jaron.command":
+                print("SR: ", packet.command)
+                text = packet.command
+
+            elif packet.DEFINITION_IDENTIFIER == "20194.exercise6.autogradesubmitresponse":
+                print("S: SUBMITRESPONSE {} {} {}".format(packet.submit_status, packet.client_status, packet.server_status)
+            else:
+                raise ValueError(packet.DEFINITION_IDENTIFIER)
+
+            time.sleep(.2)
+            lines = text.split("<EOL>\n")
+            #if self.game.status == "playing":
+            for line in lines:
+                if len(line) > 0:
+                    print("S: ", line)
+                    self.game.command(line)
+                    self.status = self.game.status
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    coro = playground.create_server(StudentServer,"localhost", 7816)
+    server = loop.run_until_complete(coro)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    server.close()
+    loop.run_until_complete(server.wait_close())
+    loop.close()
