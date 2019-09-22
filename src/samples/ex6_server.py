a6f909eb8d3f5162b9d8e0f74523ea6dc2fd9122
Jaron Lee <jaron2005@gmail.com>
2019-09-22 00:42:47 -0400

    fix port

--- src/samples/ex6_server.py
+++ src/samples/ex6_server.py
@@ -70,7 +70,7 @@ class StudentServer(asyncio.Protocol):
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    coro = playground.create_server(StudentServer,"localhost", 7821)
+    coro = playground.create_server(StudentServer,"localhost", 7822)
     server = loop.run_until_complete(coro)
 
     try:
