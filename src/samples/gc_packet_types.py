8729d3feccdc830b2d661222fb38a35f90ef1673
Jaron Lee <jaron2005@gmail.com>
2019-09-28 18:39:39 -0400

    no methods

--- src/samples/gc_packet_types.py
+++ src/samples/gc_packet_types.py
@@ -19,7 +19,7 @@ def process_game_init(pkt):
     Returns username from game init packet
 
     """
-    return pkt.get_username()
+    return pkt.username
 
 def create_game_require_pay_packet(unique_id, account, amount):
     """
@@ -108,12 +108,7 @@ class GameCommandPacket(PacketType):
         # whatever you want here
     ]
 
-    @classmethod
-    def create_game_command_packet(cls, s):
-        return cls(server_command=s)# whatever arguments needed to construct the packet)
     
-    def command(self):
-        return self.server_command# whatever you need to get the command for the game
     
 class GameResponsePacket(PacketType):
     DEFINITION_IDENTIFIER = "responsepacket" 
@@ -125,17 +120,4 @@ class GameResponsePacket(PacketType):
             ("server_status", STRING),
         # whatever you want here
     ]
-
-    @classmethod
-    def create_game_response_packet(cls, response, status):
-        return cls(server_response=response, server_status=status) # whatever you need to construct the packet )
-    
-    def game_over(self):
-        return self.server_status in ("dead", "escaped")# whatever you need to do to determine if the game is over
-    
-    def status(self):
-        return self.server_status# whatever you need to do to return the status
-    
-    def response(self):
-        return self.server_response# whatever you need to do to return the response
 
