2614b4638dbaae8079403324f3401058bcc58c80
Jaron Lee <jaron2005@gmail.com>
2019-09-29 18:59:40 -0400

    removed redundant

--- src/samples/ex7_client.py
+++ src/samples/ex7_client.py
@@ -16,8 +16,6 @@ EnablePresetLogging(PRESET_DEBUG)
 bankconfig = OnlineBankConfig()
 bank_addr =     bankconfig.get_parameter("CLIENT", "bank_addr")
 bank_port = int(bankconfig.get_parameter("CLIENT", "bank_port"))
-bank_stack     =     bankconfig.get_parameter("CLIENT", "stack","default")
-bank_username  =     bankconfig.get_parameter("CLIENT", "username")
 
 certPath = os.path.join(bankconfig.path(), "bank.cert")
 bank_cert = loadCertFromFile(certPath)
